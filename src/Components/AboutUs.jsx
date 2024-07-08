7a91be3405945b932199252eeee68febcf9cff5e
JorgeLG87 <jorge85_6@hotmail.com>
2024-07-08 14:53:19 -0400

    Added profile pic and links in the about us page

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -2,8 +2,20 @@ import "./AboutUs.css";
 
 export default function AboutUs() {
     return (
-        <>
-        <p>Working</p>
-        </>
+        <div className="aboutUs-main-container">
+            < div className="title-container">
+                <p className="title">Jorge Godoy</p>
+            </div>
+            <img className="profile-image" src="/professionalpic.jpeg" alt="profile pic"/>
+            <div className="links-container">
+                <img className="links-icon" src="/github.svg"/>
+                <p className="links-text">Github Account</p>
+            </div>
+            <div className="links-container">
+                <img className="links-icon" src="/linkedin.svg"/>
+                <p className="links-text">LinkedIn Account</p>
+            </div>
+        </div>
     )
 }
+
